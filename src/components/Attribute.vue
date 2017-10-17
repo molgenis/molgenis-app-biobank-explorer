238da3c4ebd8e1ba8d50300c2e04016766943636
Mark-de-Haan <markdehaan90@gmail.com>
2017-10-17 12:50:47 +0200

    Format Attribute.vue

--- src/components/Attribute.vue
+++ src/components/Attribute.vue
@@ -1,17 +1,30 @@
 <template>
-  <div v-if="attribute.fieldType != 'COMPOUND'"> <!--TODO: implement compound-->
-    <b>{{attribute.label}}: </b>
-    <a v-if="attribute.fieldType==='HYPERLINK'" :href="value">{{value}}</a>
-    <span v-else-if="attribute.fieldType==='BOOL'">
-      <i class="fa fa-check" aria-hidden="true" v-if="value===true"></i>
-      <i class="fa fa-times" aria-hidden="true" v-else></i>
-    </span>
-    <span v-else-if="attribute.fieldType==='XREF' | attribute.fieldType==='CATEGORICAL'">{{value[getRefLabel()]}}</span>
-    <ul v-else-if="attribute.fieldType==='MREF' | attribute.fieldType==='CATEGORICAL_MREF'">
-      <li v-for="val in value">{{val[getRefLabel()]}}</li>
-      <li v-if="value.length === 0">None</li>
-    </ul>
-    <span v-else>{{value}}<span v-show="!value">-</span></span>
+  <div>
+    <div v-if="attribute.fieldType != 'COMPOUND'"> <!--TODO: implement compound-->
+      <b>{{attribute.label}}: </b>
+
+      <a v-if="attribute.fieldType==='HYPERLINK'" :href="value">{{value}}</a>
+
+      <span v-else-if="attribute.fieldType==='BOOL'">
+        <i class="fa fa-check" aria-hidden="true" v-if="value===true"></i>
+        <i class="fa fa-times" aria-hidden="true" v-else></i>
+      </span>
+
+      <span v-else-if="attribute.fieldType==='XREF' || attribute.fieldType==='CATEGORICAL'">
+        {{value[getRefLabel()]}}
+      </span>
+
+      <ul v-else-if="attribute.fieldType==='MREF' || attribute.fieldType==='CATEGORICAL_MREF'">
+        <li v-for="val in value">{{val[getRefLabel()]}}</li>
+        <li v-if="value.length === 0">None</li>
+      </ul>
+
+      <span v-else>{{value}}<span v-show="!value">-</span></span>
+    </div>
+
+    <div v-else>
+
+    </div>
   </div>
 </template>
 
@@ -31,9 +44,7 @@
     props: ['attribute', 'value'],
     methods: {
       getRefLabel: function () {
-        let self = this
-        const label = self.attribute.refEntity.attributes.find(attr => attr.labelAttribute).name
-        return label
+        return self.attribute.refEntity.attributes.find(attr => attr.labelAttribute).name
       }
     }
   }
