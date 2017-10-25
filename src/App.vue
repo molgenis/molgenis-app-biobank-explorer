f83841a7da3ca2b6097d15693a685d733ead24e4
Mark-de-Haan <markdehaan90@gmail.com>
2017-10-25 17:15:34 +0200

    Add a cool app title

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,38 @@
 <template>
   <div class="container">
+    <h5>
+      <span class="line-center">BBMRI-ERIC Directory</span>
+    </h5>
     <router-view></router-view>
   </div>
 </template>
+
+<style scoped>
+  .line-center {
+    margin: 0;
+    padding: 0 10px;
+    background: #fff;
+    display: inline-block;
+  }
+
+  h5 {
+    margin-top: 1em;
+    text-align: center;
+    position: relative;
+    z-index: 2;
+
+  }
+
+  h5:after {
+    content: "";
+    position: absolute;
+    top: 50%;
+    left: 0;
+    right: 0;
+    border-top: solid 1px black;
+    z-index: -1;
+  }
+</style>
 
 <script>
   export default {
