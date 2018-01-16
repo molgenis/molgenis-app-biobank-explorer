8d00c11b5f5177188fdc3ae5a83e397eb594206f
Mark-de-Haan <markdehaan90@gmail.com>
2018-01-16 10:51:15 +0100

    Make menu links runtime configurable

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" :href="home">
-        <img class="img-responsive" width="140" height="35" src="https://molgenis114.gcc.rug.nl/logo/bbmri-eric-logo-ohnezusatz.png" alt="BBMRI-ERIC">
+      <a class="navbar-brand" :href="homeLink">
+        <img class="img-responsive" width="140" height="35"
+             :src="imageSourceLink" alt="BBMRI-ERIC">
       </a>
 
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-molgenis-navbar" aria-controls="bs-molgenis-navbar" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-molgenis-navbar"
+              aria-controls="bs-molgenis-navbar" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
@@ -13,42 +15,42 @@
         <ul class="navbar-nav mr-auto">
           <li class="nav-item">
             <a class="nav-link"
-              :href="home + '/menu/main/apps/aaaacx4r5ehjr6qwhylusdiaai'">
+               :href="directoryLink">
               Directory 4.0
             </a>
           </li>
 
           <li class="nav-item">
             <a class="nav-link"
-              :href="home + '/menu/main/dataexplorer?entity=eu_bbmri_eric_collections&amp;mod=data&amp;attrs[]=country&amp;attrs[]=biobank&amp;attrs[]=collection&amp;hideselect=true'">
+               :href="advancedDirectoryLink">
               Directory 4.0 (Advanced search)
             </a>
           </li>
 
           <li class="nav-item">
             <a class="nav-link"
-              :href="home + '/menu/main/dataexplorer?entity=eu_bbmri_eric_collections&amp;mod=data&amp;attrs%5B%5D=country&amp;attrs%5B%5D=biobank&amp;attrs%5B%5D=collection&amp;hideselect=true&amp;filter=type==RD'">
+               :href="rareDiseaseBiobanksLink">
               Rare Disease Biobanks
             </a>
           </li>
 
           <li class="nav-item">
             <a class="nav-link"
-              :href="home + '/menu/main/redirect?url=https://drive.google.com/file/d/0B0PMkmx_peQ7cmhkdFBncTRzSTg/view?usp=sharing'">
+               :href="userManualLink">
               User Manual
             </a>
           </li>
 
           <li class="nav-item">
             <a class="nav-link"
-               :href="home + '/menu/main/references'">
+               :href="referencesLink">
               References
             </a>
           </li>
 
           <li class="nav-item">
             <a class="nav-link"
-               :href="home + '/menu/main/feedback'">
+               :href="feedbackLink">
               Feedback
             </a>
           </li>
@@ -68,7 +70,14 @@
     name: 'biobank-explorer',
     data () {
       return {
-        home: INITIAL_STATE.serverUrl
+        homeLink: INITIAL_STATE.serverUrl,
+        imageSourceLink: INITIAL_STATE.imageSourceLink,
+        directoryLink: INITIAL_STATE.menuLinks.directoryLink,
+        advancedDirectoryLink: INITIAL_STATE.menuLinks.advancedDirectoryLink,
+        rareDiseaseBiobanksLink: INITIAL_STATE.menuLinks.rareDiseaseBiobanksLink,
+        userManualLink: INITIAL_STATE.menuLinks.userManualLink,
+        referencesLink: INITIAL_STATE.menuLinks.referencesLink,
+        feedbackLink: INITIAL_STATE.menuLinks.feedbackLink
       }
     }
   }
