ac09eb16afd92ea9af883c374e40c13b010ef15e
sido <sido@haakma.org>
2018-08-01 16:10:24 +0200

    flatmap is moved back into expirimental features and not supported anymore

--- src/components/tables/CollectionsTable.vue
+++ src/components/tables/CollectionsTable.vue
@@ -80,7 +80,7 @@
     },
     computed: {
       topLevelElements () {
-        const subCollectionIds = this.collections.flatMap(collection => collection.sub_collections.map(subCollection => subCollection.id))
+        const subCollectionIds = this.collections.map(collection => collection.sub_collections.map(subCollection => subCollection.id))
         return this.collections.filter(collection => subCollectionIds.indexOf(collection.id) === -1)
       }
     },
