8c1be7c3fd8580fade61c2edb89ee0f0ddfd5ed0
Fleur Kelpin <f.kelpin@umcg.nl>
2018-08-14 22:46:49 +0200

    fix (actions): Always fetch collection ids to prevent postgres error

--- src/store/actions.js
+++ src/store/actions.js
@@ -118,7 +118,7 @@ export default {
 
     const query = helpers.createRSQLQuery(state)
 
-    api.get(`${COLLECTION_API_PATH}?num=10000&attrs=biobank${query}`).then(response => {
+    api.get(`${COLLECTION_API_PATH}?num=10000&attrs=~id,biobank${query}`).then(response => {
       dispatch(GET_BIOBANKS_BY_ID, response.items)
     }, error => {
       commit(SET_ERROR, error)
