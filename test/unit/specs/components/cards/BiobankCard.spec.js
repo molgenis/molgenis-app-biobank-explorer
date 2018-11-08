f12d677d18c1ba4f16ece7bed10eeee42794899b
connor <c.stroomberg@gmail.com>
2018-11-08 11:05:36 +0100

    Add missing stubs for router to avoid error messages in test output

--- test/unit/specs/components/cards/BiobankCard.spec.js
+++ test/unit/specs/components/cards/BiobankCard.spec.js
@@ -4,6 +4,7 @@ import BiobankCard from '@/components/cards/BiobankCard'
 
 describe('BiobankCard', () => {
   let propsData
+  let stubs
 
   beforeEach(() => {
     propsData = {
@@ -15,21 +16,22 @@ describe('BiobankCard', () => {
         ]
       }
     }
+    stubs = ['router-link', 'router-view']
   })
 
   it('should initialize the collapsed prop to true if none is passed', () => {
-    const wrapper = shallowMount(BiobankCard, {propsData})
+    const wrapper = shallowMount(BiobankCard, {propsData, stubs})
     expect(wrapper.props().initCollapsed).to.equal(true)
   })
 
   it('should use the passed collapsed setting if passed', () => {
     propsData.initCollapsed = false
-    const wrapper = shallowMount(BiobankCard, {propsData})
+    const wrapper = shallowMount(BiobankCard, {propsData, stubs})
     expect(wrapper.props().initCollapsed).to.equal(false)
   })
 
   it('should expose a list of collectionTypes', () => {
-    const wrapper = shallowMount(BiobankCard, {propsData})
+    const wrapper = shallowMount(BiobankCard, {propsData, stubs})
     expect(wrapper.vm.collectionTypes).to.equal('col-type-a, col-type-b')
   })
 })
