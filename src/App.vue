47227e50e23144db1d79b515f347a62072a036ed
Fleur Kelpin <f.kelpin@umcg.nl>
2019-12-02 13:59:06 +0100

    feat: show error alert

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <div class="container">
+      <b-alert v-if="error" show variant="danger" dismissible>{{errorMessage}}</b-alert>
       <router-view></router-view>
     </div>
   </div>
@@ -13,7 +14,23 @@
 </style>
 
 <script>
+  import { mapState } from 'vuex'
   export default {
+    computed: {
+      ...mapState(['error']),
+      errorMessage () {
+        if (!this.error) {
+          return ''
+        }
+        if (this.error.errors) {
+          return this.error.errors[0].message
+        }
+        if (this.error.message) {
+          return this.error.message
+        }
+        return 'Something went wrong'
+      }
+    },
     name: 'biobank-explorer'
   }
 </script>
