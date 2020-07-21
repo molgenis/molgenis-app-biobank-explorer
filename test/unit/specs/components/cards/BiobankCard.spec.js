6d9ce55e3208c925c3c7fc9092837c068cf1000d
Jelmer Veen <j.p.veen@rug.nl>
2020-07-21 13:52:21 +0200

    chore: added unittest

--- test/unit/specs/components/cards/BiobankCard.spec.js
+++ test/unit/specs/components/cards/BiobankCard.spec.js
@@ -11,9 +11,18 @@ describe('BiobankCard', () => {
       biobank: {
         id: 'biobank-1',
         collections: [{
+          name: 'z',
           type: [{label: 'col-type-a'}],
           sub_collections: [{
             type: [{label: 'col-type-b'}],
+            sub_collections: []
+          }]
+        },
+        {
+          name: 'a',
+          type: [{label: 'col-type-d'}],
+          sub_collections: [{
+            type: [{label: 'col-type-e'}],
             sub_collections: []
           }]
         }]
@@ -35,7 +44,7 @@ describe('BiobankCard', () => {
 
   it('should expose a list of collectionTypes', () => {
     const wrapper = shallowMount(BiobankCard, {propsData, stubs})
-    expect(wrapper.vm.collectionTypes).to.equal('col-type-a, col-type-b')
+    expect(wrapper.vm.collectionTypes).to.equal('col-type-a, col-type-b, col-type-d, col-type-e')
   })
   it('should expose a list of covid19 types if available', () => {
     propsData.biobank.covid19biobank = [{label: 'Member of the COVID-19 network'}, {name: 'COVID-19'}]
@@ -45,5 +54,26 @@ describe('BiobankCard', () => {
   it('should return nothing if covid types are not available', () => {
     const wrapper = shallowMount(BiobankCard, {propsData, stubs})
     expect(wrapper.vm.availableCovidTypes).to.equal('')
+  })
+  it('should sort collections based by name alphabeticaly', () => {
+    const result = [{
+      name: 'a',
+      type: [{label: 'col-type-d'}],
+      sub_collections: [{
+        type: [{label: 'col-type-e'}],
+        sub_collections: []
+      }]
+    },
+    {
+      name: 'z',
+      type: [{label: 'col-type-a'}],
+      sub_collections: [{
+        type: [{label: 'col-type-b'}],
+        sub_collections: []
+      }]
+    }]
+
+    const wrapper = shallowMount(BiobankCard, {propsData, stubs})
+    expect(wrapper.vm.sortedCollections).to.deep.equal(result)
   })
 })
