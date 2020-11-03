5ae5d183830cad54252279003cb3083fdb3279a1
Jelmer Veen <j.p.veen@rug.nl>
2020-11-03 17:19:19 +0100

    chore: excluded helpers from tests

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   transformIgnorePatterns: ['/node_modules/(?!@molgenis/molgenis-api-client|@molgenis/molgenis-vue-test-utils)'],
   moduleNameMapper: {
     '\\.(css)$': '<rootDir>/tests/stylemock/styleMock.js'
-  }
+  },
+  testPathIgnorePatterns: ['/src/utils/filter*.js']
 }
