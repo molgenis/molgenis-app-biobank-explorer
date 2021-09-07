molgenis/biobank-explorer

fix: fix create bookmark: now it doesn't include filters that are not displayed

src/components/filters/FilterContainer.vue
@@ -69,7 +69,7 @@ export default {
   methods: {
     ...mapMutations(['UpdateFilter']),
     filterChange (name, value) {
-      this.UpdateFilter({ name, value, router: this.$router })
+      this.UpdateFilter({ name, value, filterDefinitions: this.filterDefinitions, router: this.$router })
     }
   }
 }

molgenis/molgenis-app-biobank-explorer

src/components/filters/FilterContainer.vue
@@ -68,7 +68,7 @@ export default {
   methods: {
     ...mapMutations(['UpdateFilterSelection', 'UpdateFilterSatisfyAll']),
     filterChange (name, value) {
-      this.UpdateFilterSelection({ name, value })
+      this.UpdateFilterSelection({ name, value, filterDefinitions: this.filterDefinitions, router: this.$router })
     },
     filterSatisfyAllChange (name, value) {
       this.UpdateFilterSatisfyAll({ name, value })