molgenis/biobank-explorer

feat: changes the biobank network filters logic
When the biobank network filters is selected, it gets the biobanks in the network and the also all
the collections in the found biobanks

src/components/filters/FilterContainer.vue
@@ -68,7 +68,7 @@ export default {
   methods: {
     ...mapMutations(['UpdateFilter']),
     filterChange (name, value) {
-      this.UpdateFilter({ name, value, filterDefinitions: this.filterDefinitions, router: this.$router })
+      this.UpdateFilter({ name, value, router: this.$router })
     }
   }
 }

molgenis/molgenis-app-biobank-explorer

src/components/filters/FilterContainer.vue
@@ -67,10 +67,10 @@ export default {
   methods: {
     ...mapMutations(['UpdateFilterSelection', 'UpdateFilterSatisfyAll']),
     filterChange (name, value) {
-      this.UpdateFilterSelection({ name, value, filterDefinitions: this.filterDefinitions, router: this.$router })
+      this.UpdateFilterSelection({ name, value, router: this.$router })
     },
     filterSatisfyAllChange (name, value) {
-      this.UpdateFilterSatisfyAll({ name, value })
+      this.UpdateFilterSatisfyAll({ name, value, router: this.$router })
     }
   }
 }