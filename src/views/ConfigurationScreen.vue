a84e70cbd3e5402cc0bea0cc56670beb760aaac0
Jelmer Veen <j.p.veen@rug.nl>
2022-03-14 15:50:47 +0100

    chore: added editor to view and loaded config file with formatting

--- src/views/ConfigurationScreen.vue
+++ src/views/ConfigurationScreen.vue
@@ -1,18 +1,53 @@
 <template>
-<span>{{ appConfig}}</span>
+  <div class="editor-container">
+    <div ref="editor" class="editor"  @keyup.ctrl.f="format"></div>
+  </div>
 </template>
 
 <script>
 import { mapActions, mapState } from 'vuex'
 export default {
+  data () {
+    return {
+      editor: {}
+    }
+  },
   methods: {
-    ...mapActions(['GetApplicationConfiguration'])
+    ...mapActions(['GetApplicationConfiguration']),
+    format () {
+      this.editor.getAction('editor.action.formatDocument').run()
+    }
   },
   computed: {
     ...mapState(['appConfig'])
   },
-  mounted () {
+  async mounted () {
     this.GetApplicationConfiguration()
+    const monaco = await import('monaco-editor')
+
+    this.editor = monaco.editor.create(this.$refs.editor, {
+      value: this.appConfig,
+      language: 'json'
+    })
+
+    this.format()
   }
 }
 </script>
+
+<style scoped >
+.editor-container {
+  margin: 0 auto;
+  width: 90vw;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  justify-content: center;
+}
+
+.editor {
+  border: 1px solid black;
+  height: 65vh;
+  width: 100%;
+}
+</style>
