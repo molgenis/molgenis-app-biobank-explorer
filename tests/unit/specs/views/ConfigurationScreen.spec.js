047607093c2bd4d98461ef5f1349732eac2265e8
Jelmer Veen <j.p.veen@rug.nl>
2022-03-18 15:07:00 +0100

    chore: add unittest for configuration screen save and undo

--- tests/unit/specs/views/ConfigurationScreen.spec.js
+++ tests/unit/specs/views/ConfigurationScreen.spec.js
@@ -2,30 +2,38 @@ import { shallowMount, createLocalVue } from '@vue/test-utils'
 import Vuex from 'vuex'
 import ConfigurationScreen from '@/views/ConfigurationScreen'
 import { mockState } from '../mockData'
+import flushPromises from 'flush-promises'
 
 const localVue = createLocalVue()
 
 localVue.use(Vuex)
 
-const GetApplicationConfiguration = jest.fn()
+const mockAppConfig = { test: 'data' }
+const GetApplicationConfiguration = jest.fn().mockReturnValue(mockAppConfig)
 const SaveApplicationConfiguration = jest.fn()
+const editorGetValue = jest.fn()
+const editorSetValue = jest.fn()
+
+const mockEditor = {
+  create: jest.fn().mockReturnValue(
+    {
+      getAction: jest.fn().mockReturnValue({ run: jest.fn() }),
+      getValue: editorGetValue,
+      getModel: jest.fn().mockReturnValue({ setValue: editorSetValue })
+    })
+}
 
 describe('ConfigurationScreen', () => {
   let store
   beforeEach(() => {
     jest.mock('monaco-editor/esm/vs/editor/editor.api', () => {
       return {
-        editor: {
-          create: jest.fn().mockReturnValue(
-            {
-              getAction: jest.fn().mockReturnValue({ run: jest.fn() })
-            })
-        }
+        editor: mockEditor
       }
     })
 
     store = new Vuex.Store({
-      state: mockState(),
+      state: { ...mockState(), appConfig: mockAppConfig },
       actions: {
         GetApplicationConfiguration,
         SaveApplicationConfiguration
@@ -36,5 +44,23 @@ describe('ConfigurationScreen', () => {
   it('should get config on mount', () => {
     shallowMount(ConfigurationScreen, { store, localVue })
     expect(GetApplicationConfiguration).toHaveBeenCalled()
+  })
+
+  it('should set statusClosed to false, get the editor value and call save when saving, so that notification can be shown', async () => {
+    const wrapper = shallowMount(ConfigurationScreen, { store, localVue })
+    await flushPromises() // needed for async mounted.
+    wrapper.vm.save()
+
+    expect(wrapper.vm.$data.statusClosed).toBeFalsy()
+    expect(editorGetValue).toHaveBeenCalled()
+    expect(SaveApplicationConfiguration).toHaveBeenCalled()
+  })
+
+  it('calls setvalue on editor and sets the appconfig back on cancel', async () => {
+    const wrapper = shallowMount(ConfigurationScreen, { store, localVue })
+    await flushPromises()
+    wrapper.vm.cancel()
+
+    expect(editorSetValue).toHaveBeenCalledWith(mockAppConfig)
   })
 })
