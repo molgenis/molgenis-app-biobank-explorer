e4110992a80483eb2819b7277ff85a0497b4c9dd
Jelmer Veen <j.p.veen@rug.nl>
2022-04-08 12:06:06 +0200

    chore: initial setup

--- src/components/cards/BiobankCard.vue
+++ src/components/cards/BiobankCard.vue
@@ -1,97 +1,107 @@
 <template>
-  <div
+  <!-- template -->
+  <article
     :class="[
       {
         'border-secondary': biobankInSelection,
         'border-light': !biobankInSelection,
       },
-      'card biobank-card shadow-sm',
+      'card biobank-card shadow-sm flip',
     ]">
-    <div v-if="loading" class="text-center p-5">
-      <span class="fa fa-spinner fa-spin" aria-hidden="true"></span>
-    </div>
-    <div v-else>
-      <header class="border-0 card-header p-1">
-        <h5 class="pt-1 pl-2 pr-1 mt-1">
-          <router-link :to="'/biobank/' + biobank.id" title="Biobank details">
-            <span class="biobank-name">{{ biobank.name }}</span>
-            <font-awesome-icon
-              class="float-right m-1 text-dark"
-              :icon="['far', 'arrow-alt-circle-right']"/>
-          </router-link>
-        </h5>
-      </header>
+    <div tabindex="0">
+      <section>
+        <div class="front">
+          <div v-if="loading" class="text-center p-5">
+            <span class="fa fa-spinner fa-spin" aria-hidden="true"></span>
+          </div>
+          <div v-else>
+            <header class="border-0 card-header p-1">
+              <h5 class="pt-1 pl-2 pr-1 mt-1">
+                <router-link
+                  :to="'/biobank/' + biobank.id"
+                  title="Biobank details">
+                  <span class="biobank-name">{{ biobank.name }}</span>
+                  <font-awesome-icon
+                    class="float-right m-1 text-dark"
+                    :icon="['far', 'arrow-alt-circle-right']"/>
+                </router-link>
+              </h5>
+            </header>
 
-      <div class="d-flex mt-2" v-if="biobank.collections.length">
-        <button
-          class="btn ml-2"
-          :class="{
-            'btn-outline-secondary': !showCollections,
-            'btn-light border': showCollections,
-          }"
-          @click.prevent="showCollections = false">
-          Biobank details
-        </button>
+            <div class="d-flex mt-2" v-if="biobank.collections.length">
+              <button
+                class="btn ml-2"
+                :class="{
+                  'btn-outline-secondary': !showCollections,
+                  'btn-light border': showCollections,
+                }"
+                @click.prevent="showCollections = false">
+                Biobank details
+              </button>
 
-        <button
-          class="btn ml-1"
-          :class="{
-            'btn-outline-secondary': showCollections,
-            'btn-light border': !showCollections,
-          }"
-          @click.prevent="showCollections = true">
-          Collections details
-        </button>
+              <button
+                class="btn ml-1"
+                :class="{
+                  'btn-outline-secondary': showCollections,
+                  'btn-light border': !showCollections,
+                }"
+                @click.prevent="showCollections = true">
+                Collections details
+              </button>
 
-        <collection-selector
-          class="text-right ml-auto mr-2 mt-1 align-self-center"
-          v-if="biobank.collections.length > 0"
-          :collectionData="biobank.collections"
-          bookmark></collection-selector>
-      </div>
-      <section v-if="!showCollections" class="p-2 pt-1 biobank-section">
-        <small>
-          <view-generator :viewmodel="biobankcardViewmodel" />
-        </small>
-      </section>
-      <section class="card-body collections-section pt-0">
-        <div
-          class="collection-items mt-4"
-          v-for="collectionDetail of biobank.collectionDetails"
-          :key="collectionDetail.id">
-          <div v-if="showCollections" class="mb-2">
-            <div class="collection-header card-header border p-2">
-              <router-link
-                :to="'/collection/' + collectionDetail.id"
-                title="Collection details">
-                <span class="collection-name">{{ collectionDetail.name }}</span>
-                <font-awesome-icon
-                  class="float-right m-1 text-dark"
-                  :icon="['far', 'arrow-alt-circle-right']"/>
-              </router-link>
+              <collection-selector
+                class="text-right ml-auto mr-2 mt-1 align-self-center"
+                v-if="biobank.collections.length > 0"
+                :collectionData="biobank.collections"
+                bookmark></collection-selector>
             </div>
-            <small>
-              <div
-                class="
-                  pt-2
-                  px-2
-                  border border-top-0 border-bottom-0
-                  d-flex
-                ">
-                <collection-selector
-                  class="ml-auto"
-                  :collectionData="collectionDetail"
-                  bookmark></collection-selector>
-              </div>
-              <view-generator
-                class="border p-2 border-top-0 pt-2"
-                :viewmodel="collectionViewmodel(collectionDetail)"/>
-            </small>
+            <section v-if="!showCollections" class="p-2 pt-1 biobank-section">
+              <small>
+                <view-generator :viewmodel="biobankcardViewmodel" />
+              </small>
+            </section>
           </div>
         </div>
       </section>
+      <section>
+        <div class="back">
+          <section class="card-body collections-section pt-0">
+            <div
+              class="collection-items mt-4"
+              v-for="collectionDetail of biobank.collectionDetails"
+              :key="collectionDetail.id">
+              <div v-if="showCollections" class="mb-2">
+                <div class="collection-header card-header border p-2">
+                  <router-link
+                    :to="'/collection/' + collectionDetail.id"
+                    title="Collection details">
+                    <span class="collection-name">{{
+                      collectionDetail.name
+                    }}</span>
+                    <font-awesome-icon
+                      class="float-right m-1 text-dark"
+                      :icon="['far', 'arrow-alt-circle-right']"/>
+                  </router-link>
+                </div>
+                <small>
+                  <div
+                    class="pt-2 px-2 border border-top-0 border-bottom-0 d-flex">
+                    <collection-selector
+                      class="ml-auto"
+                      :collectionData="collectionDetail"
+                      bookmark></collection-selector>
+                  </div>
+                  <view-generator
+                    class="border p-2 border-top-0 pt-2"
+                    :viewmodel="collectionViewmodel(collectionDetail)"/>
+                </small>
+              </div>
+            </div>
+          </section>
+        </div>
+      </section>
     </div>
-  </div>
+  </article>
 </template>
 
 <script>
@@ -138,10 +148,7 @@ export default {
     }
   },
   computed: {
-    ...mapState([
-      'biobankColumns',
-      'collectionColumns'
-    ]),
+    ...mapState(['biobankColumns', 'collectionColumns']),
     ...mapGetters(['selectedCollections']),
     biobankcardViewmodel () {
       // check if biobank is still loading
@@ -213,6 +220,93 @@ export default {
 
 /* TODO put in theme */
 .card-header {
-    background-color: #efefef;
+  background-color: #efefef;
 }
+
+/** Flip card */
+article {
+  padding: 1.5rem;
+}
+article > *:not(header):not(footer):not(:last-child) {
+  min-height: auto;
+  margin-bottom: 1.5rem;
+}
+article > *:nth-child(2) {
+  margin-top: 1.5rem;
+  padding: 0;
+}
+article header,
+article footer {
+  width: 100%;
+  min-height: auto;
+  padding-bottom: 1.5rem;
+}
+article header > *,
+article footer > * {
+  margin: 0;
+}
+article footer {
+  padding: 1.5rem 0 0 0;
+}
+article.flip {
+  padding: 0;
+  position: relative;
+  width: 22rem;
+  height: 30rem;
+  perspective: 1000px;
+}
+
+article.flip [tabindex="0"] {
+  box-shadow: 0 6.4px 14.4px 0 rgba(0, 0, 0, 0.132),
+    0 1.2px 3.6px 0 rgba(0, 0, 0, 0.108);
+}
+article.flip [tabindex="0"] section {
+  background-color: #fff;
+  border: 0.1px solid #fff;
+}
+
+article.flip:hover > [tabindex="0"] {
+  transform: rotateY(180deg);
+}
+article.flip [tabindex="0"] {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
+  -webkit-transform-style: preserve-3d;
+}
+article.flip [tabindex="0"]:focus {
+  transform: rotateY(180deg);
+}
+article.flip [tabindex="0"] section {
+  position: absolute;
+  flex-flow: column;
+  align-items: flex-start;
+  justify-content: start;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  /* Safari */
+  backface-visibility: hidden;
+  box-sizing: border-box;
+}
+article.flip [tabindex="0"] section div {
+  word-break: break-word;
+  white-space: normal;
+  overflow: auto;
+}
+article.flip [tabindex="0"] section header {
+  padding: 0 0 1.5rem 0;
+}
+article.flip [tabindex="0"] section footer {
+  margin-top: auto;
+  padding: 1.5rem 0 0 0;
+  min-height: unset;
+}
+article.flip [tabindex="0"] section:last-child {
+  transform: rotateY(180deg);
+}
+
+/** ~~~ */
 </style>
