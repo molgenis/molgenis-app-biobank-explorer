082503887d13121219ddd4e5461ebfff32a61cda
Jelmer Veen <j.p.veen@rug.nl>
2022-06-24 15:34:45 +0200

    feat: add icon to indicate biobank having quality labels

--- src/components/cards/BiobankCard.vue
+++ src/components/cards/BiobankCard.vue
@@ -25,6 +25,15 @@
                   class="fa fa-server mr-2 text-primary"
                   aria-hidden="true"></span>
                 <span class="biobank-name">{{ biobank.name }}</span>
+                <sup
+                  v-if="hasBiobankQuality"
+                  class="
+                    fa fa-check-circle-o
+                    text-success
+                    certificate-icon
+                    ml-1
+                  "
+                  aria-hidden="true"></sup>
               </router-link>
             </h5>
           </header>
@@ -65,6 +74,15 @@
                   class="fa fa-server mr-2 text-primary"
                   aria-hidden="true"></span>
                 <span class="biobank-name">{{ biobank.name }}</span>
+                <sup
+                  v-if="hasBiobankQuality"
+                  class="
+                    fa fa-check-circle-o
+                    text-success
+                    certificate-icon
+                    ml-1
+                  "
+                  aria-hidden="true"></sup>
               </router-link>
             </h5>
           </header>
@@ -235,6 +253,10 @@ export default {
       }
       return { attributes }
     },
+    hasBiobankQuality () {
+      return this.biobankcardViewmodel.attributes.some(attr => attr.type === 'quality' && attr.value.length)
+    },
+    /** broken */
     biobankInSelection () {
       if (!this.biobank.collections) return false
 
@@ -260,6 +282,10 @@ export default {
   position: relative;
   top: 0.25em;
   clip-path: inset(-15% 0% 75% 0%);
+}
+
+.certificate-icon {
+  font-size: 0.8rem;
 }
 </style>
 
