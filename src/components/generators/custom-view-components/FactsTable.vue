7232b2a0837a848d23ebb438e80a6ae8acb4a4fc
Jelmer Veen <j.p.veen@rug.nl>
2022-11-15 11:34:04 +0100

    chore: processed first feedback

--- src/components/generators/custom-view-components/FactsTable.vue
+++ src/components/generators/custom-view-components/FactsTable.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="mt-4" v-if="attribute.value && attribute.value.length">
-    <h3>Facts sheet</h3>
     <table class="table border w-100">
       <thead>
         <tr>
-          <th>Material type:</th>
-          <th>Samples:</th>
-          <th>Donors:</th>
-          <th>Disease codes:</th>
+          <th>Material type</th>
+          <th>Samples</th>
+          <th>Sex</th>
+          <th>Donors</th>
+          <th>Disease codes</th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="fact of attribute.value" :key="fact.id">
+        <template v-for="fact of attribute.value" >
+        <tr :key="fact.id" v-if="hasAFactToShow(fact)">
           <th scope="row" class="pr-1 align-top text-nowrap">
             {{ fact.sample_type.label }}
           </th>
-          <td>{{ fact.number_of_samples }}</td>
-          <td>{{ fact.number_of_donors }}</td>
-          <td>
+          <td>{{ fact.number_of_samples || '-' }}</td>
+          <td>{{ fact.sex || '-' }}</td>
+          <td>{{ fact.number_of_donors || '-' }}</td>
+          <td v-if="fact.disease && fact.disease.length">
             <div
               v-for="disease in fact.disease"
               :key="disease.id"
@@ -25,7 +27,11 @@
               {{ disease.id }}
             </div>
           </td>
+          <td v-else>
+            -
+          </td>
         </tr>
+        </template>
       </tbody>
     </table>
   </div>
@@ -38,8 +44,15 @@ export default {
       type: Object
     }
   },
-  mounted () {
-    console.log(this.attribute)
+  methods: {
+    hasAFactToShow (fact) {
+      const hasSamples = fact.number_of_samples && parseInt(fact.number_of_samples) !== 0
+      const hasDonors = fact.number_of_donors && parseInt(fact.number_of_samples) !== 0
+      const facts = [hasSamples, hasDonors, fact.sex]
+
+      /** return true, if any of the facts is filled it. */
+      return facts.some(fact => fact)
+    }
   }
 }
 </script>
