608cfd1e697c5a8d2019aaabf3ef14640b47a464
Jelmer Veen <j.p.veen@rug.nl>
2022-12-21 16:40:58 +0100

    chore: add filtering and counts of samples and donors on top of the table

--- src/components/generators/custom-view-components/FactsTable.vue
+++ src/components/generators/custom-view-components/FactsTable.vue
@@ -1,8 +1,20 @@
 <template>
   <div class="mt-4" v-if="attribute.value && attribute.value.length">
+    <table class="mb-4">
+      <tbody>
+        <tr>
+          <th>Number of samples:</th>
+          <td class="pl-4">{{ numberOfSamples }}</td>
+        </tr>
+        <tr>
+          <th>Number of donors:</th>
+          <td class="pl-4">{{ numberOfDonors }}</td>
+        </tr>
+      </tbody>
+    </table>
     <table class="table border w-100">
       <thead>
-        <tr>
+        <tr class="facts-header bg-info text-white">
           <th @click="sort('sample_type.label')">Material type</th>
           <th @click="sort('number_of_samples')">Samples</th>
           <th @click="sort('sex.CollectionSex')">Sex</th>
@@ -10,9 +22,48 @@
           <th @click="sort('age.CollectionAgeRange')">Age range</th>
           <th @click="sort('disease.id')">Disease codes</th>
         </tr>
+        <tr class="filter-bar">
+          <th>
+            <select @change="filter('sample_type.label', $event)" class="w-100">
+              <option value="all">All</option>
+              <option
+                v-for="material of materialtypeOptions"
+                :key="material"
+                :value="material">
+                {{ material }}
+              </option>
+            </select>
+          </th>
+          <th></th>
+          <th>
+            <select
+              @change="filter('sex.CollectionSex', $event)"
+              class="text-right">
+              <option value="all">All</option>
+              <option v-for="sex of sexOptions" :key="sex" :value="sex">
+                {{ sex }}
+              </option>
+            </select>
+          </th>
+          <th></th>
+          <th>
+            <select
+              @change="filter('age.CollectionAgeRange', $event)"
+              class="text-right">
+              <option value="all">All</option>
+              <option
+                v-for="ageRange of ageRangeOptions"
+                :key="ageRange"
+                :value="ageRange">
+                {{ ageRange }}
+              </option>
+            </select>
+          </th>
+          <th></th>
+        </tr>
       </thead>
       <tbody>
-        <template v-for="fact of facts">
+        <template v-for="fact of factsTable">
           <tr :key="fact.id" v-if="hasAFactToShow(fact)">
             <th scope="row" class="pr-1 align-top text-nowrap">
               {{ fact.sample_type.label }}
@@ -32,26 +83,6 @@
             <td v-else>-</td>
           </tr>
         </template>
-        <tr>
-          <th>Totals:</th>
-          <td>
-            {{
-              facts
-                .map((fact) => parseInt(fact.number_of_samples))
-                .reduce((a, b) => a + b)
-            }}
-          </td>
-          <td></td>
-          <td>
-            {{
-              facts
-                .map((fact) => parseInt(fact.number_of_donors))
-                .reduce((a, b) => a + b)
-            }}
-          </td>
-          <td></td>
-          <td></td>
-        </tr>
       </tbody>
     </table>
   </div>
@@ -68,7 +99,61 @@ export default {
     return {
       facts: [],
       sortColumn: '',
-      sortAsc: false
+      sortAsc: false,
+      filters: []
+    }
+  },
+  computed: {
+    materialtypeOptions () {
+      return [
+        ...new Set(this.attribute.value.map(attr => attr.sample_type.label))
+      ]
+    },
+    numberOfSamples () {
+      if (this.factsTable.length === 0) return 0
+
+      return this.factsTable
+        .map(attr => parseInt(attr.number_of_samples))
+        .reduce((a, b) => a + b)
+    },
+    numberOfDonors () {
+      if (this.factsTable.length === 0) return 0
+
+      return this.factsTable
+        .map(attr => parseInt(attr.number_of_donors))
+        .reduce((a, b) => a + b)
+    },
+    sexOptions () {
+      return [
+        ...new Set(this.attribute.value.map(attr => attr.sex.CollectionSex))
+      ]
+    },
+    ageRangeOptions () {
+      return [
+        ...new Set(
+          this.attribute.value.map(attr => attr.age.CollectionAgeRange)
+        )
+      ]
+    },
+    factsTable () {
+      if (this.filters.length === 0) return this.facts
+      const filteredFacts = []
+
+      const lastFilterIndex = this.filters.length - 1
+
+      for (const fact of this.facts) {
+        for (const [index, filter] of this.filters.entries()) {
+          const propertyValue = this.getPropertyValue(fact, filter.column)
+          /** it did not match all filters, so goodbye. */
+          if (propertyValue !== filter.value) {
+            break
+          } else if (index === lastFilterIndex) {
+            filteredFacts.push(fact)
+          }
+        }
+      }
+
+      return filteredFacts
     }
   },
   methods: {
@@ -82,6 +167,17 @@ export default {
       /** return true, if any of the facts is filled it. */
       return facts.some(fact => fact)
     },
+    filter (column, event) {
+      const indexToRemove = this.filters.findIndex(fa => fa.column === column)
+
+      if (indexToRemove > -1) {
+        this.filters.splice(indexToRemove, 1)
+      }
+
+      if (event.target.value !== 'all') {
+        this.filters.push({ column, value: event.target.value })
+      }
+    },
     sort (column) {
       /** user clicked again */
       if (this.sortColumn === column) {
@@ -91,36 +187,17 @@ export default {
         this.sortAsc = true
       }
 
-      const trail = column.split('.')
-
       this.facts.sort((factA, factB) => {
-        let factValueA, factValueB
+        const factAProperty = this.getPropertyValue(factA, column)
+        const factBProperty = this.getPropertyValue(factB, column)
 
-        if (trail.length === 1) {
-          factValueA = isNaN(factA[trail[0]])
-            ? factA[trail[0]]
-            : parseInt(factA[trail[0]])
+        const factValueA = isNaN(factAProperty)
+          ? factAProperty
+          : parseInt(factAProperty)
 
-          factValueB = isNaN(factB[trail[0]])
-            ? factB[trail[0]]
-            : parseInt(factB[trail[0]])
-        } else if (trail.length === 2) {
-          factValueA = isNaN(factA[trail[0]][trail[1]])
-            ? factA[trail[0]][trail[1]]
-            : parseInt(factA[trail[0]][trail[1]])
-
-          factValueB = isNaN(factB[trail[0]][trail[1]])
-            ? factB[trail[0]][trail[1]]
-            : parseInt(factB[trail[0]][trail[1]])
-        } else {
-          factValueA = isNaN(factA[trail[0]][trail[1]][trail[2]])
-            ? factA[trail[0]][trail[1]][trail[2]]
-            : parseInt(factA[trail[0]][trail[1]][trail[2]])
-
-          factValueB = isNaN(factB[trail[0]][trail[1]][trail[2]])
-            ? factB[trail[0]][trail[1]][trail[2]]
-            : parseInt(factB[trail[0]][trail[1]][trail[2]])
-        }
+        const factValueB = isNaN(factBProperty)
+          ? factBProperty
+          : parseInt(factBProperty)
 
         if (factValueA > factValueB) {
           return this.sortAsc ? 1 : -1
@@ -130,6 +207,23 @@ export default {
 
         return 0
       })
+    },
+    getPropertyValue (object, propertyString) {
+      const trail = propertyString.split('.')
+      const trailLength = trail.length
+
+      // could be recursive, but out of scope for now
+      switch (trailLength) {
+        case 1: {
+          return object[trail[0]]
+        }
+        case 2: {
+          return object[trail[0]][trail[1]]
+        }
+        case 3: {
+          return object[trail[0]][trail[1]][trail[2]]
+        }
+      }
     }
   },
   mounted () {
@@ -144,7 +238,16 @@ tr td:not(:first-child) {
   text-align: right;
 }
 
-thead tr th:hover {
+.facts-header th:hover {
   cursor: pointer;
+  opacity: 0.8;
+}
+
+.facts-header th {
+  border-bottom: none;
+}
+
+.filter-bar th {
+  border-top: none;
 }
 </style>
