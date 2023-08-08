da737c5753869180c4fae36a077c87b67e0e43a2
Jelmer Veen <jelmerveen@pennions.com>
2023-08-08 10:07:29 +0200

    fix: facts datamodel changes for sex and age range (#418)
    Co-authored-by: Jelmer Veen <j.p.veen@rug.nl>

--- vue.config.js
+++ vue.config.js
@@ -33,7 +33,7 @@ const htmlTemplate = () => {
   if (process.env.NODE_ENV === 'test') return 'public/preview.html'
 }
 
-const PROXY_TARGET = 'https://bbmri-acc.gcc.rug.nl/'
+const PROXY_TARGET = 'https://bbmri-facts.molgeniscloud.org/'
 
 const apiDevServerProxyConf = {
   target: PROXY_TARGET,
